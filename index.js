4d4605091dd47b606a10ea7ee2e62deddd7b6b0a
Adam <inmon.adam@yahoo.com>
2021-10-31 16:13:42 -0500

    commit tree 61f85ddd7f95c7b84f7f1e9bb9014eac6616d6e5

--- index.js
+++ index.js
@@ -1,1 +1,84 @@
 // Your code here
+function createEmployeeRecord(array){
+    let newObj = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return newObj;
+}
+
+function createEmployeeRecords(array){
+    return array.map(employee => createEmployeeRecord(employee));
+}
+
+function createTimeInEvent(employee, dateStamp){
+    let [date, hour] = dateStamp.split(' ');
+
+    employee.timeInEvents.push({
+        type: 'TimeIn',
+        hour: parseInt(hour, 10),
+        date
+    })
+
+    return employee;
+}
+
+function createTimeOutEvent(employee, dateStamp){
+    let [date, hour] = dateStamp.split(' ');
+
+    employee.timeOutEvents.push({
+        type: 'TimeOut',
+        hour: parseInt(hour, 10),
+        date
+    })
+
+    return employee;
+}
+
+let hoursWorkedOnDate = function(employee, dateStamp){
+    let inEvent = employee.timeInEvents.find(function(e){
+        return e.date === dateStamp
+    })
+
+    let outEvent = employee.timeOutEvents.find(function(e){
+        return e.date === dateStamp
+    })
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+let wagesEarnedOnDate = function(employee, dateStamp){
+    let rawWage = hoursWorkedOnDate(employee, dateStamp)
+        * employee.payPerHour
+    return parseFloat(rawWage.toString())
+}
+
+
+
+let allWagesFor = function(employee){
+    let eligibleDates = employee.timeInEvents.map(function(e){
+        return e.date
+    })
+
+    let payable = eligibleDates.reduce(function(memo, d){
+        return memo + wagesEarnedOnDate(employee, d)
+    }, 0)
+
+    return payable
+}
+
+let findEmployeeByFirstName = function(srcArray, firstName) {
+    return srcArray.find(function(rec){
+      return rec.firstName === firstName
+    })
+  }
+  
+  let calculatePayroll = function(arrayOfEmployeeRecords){
+      return arrayOfEmployeeRecords.reduce(function(memo, rec){
+          return memo + allWagesFor(rec)
+      }, 0)
+  }
